9331ae1dfded009128521b1a5fe4dff680463703
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2025-01-16 20:05:36 +0900

    refactor: 주석 삭제 및 자료형 네임스페이스 삭제

--- Engine/Graphic.cpp
+++ Engine/Graphic.cpp
@@ -4,8 +4,6 @@
 LRESULT CALLBACK
 MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	// Forward hwnd on because we can get messages (e.g., WM_CREATE)
-	// before CreateWindow returns, and thus before mhMainWnd is valid.
 	return GRAPHIC->MsgProc(hwnd, msg, wParam, lParam);
 }
 
@@ -36,7 +34,6 @@ void Graphic::Set4xMsaaState(bool value)
 	{
 		_appDesc._4xMsaaState = value;
 
-		// Recreate the swapchain and buffers with new multisample settings.
 		CreateSwapChain();
 		OnResize();
 	}
@@ -60,7 +57,6 @@ bool Graphic::Initialize()
 	if (!InitDirect3D())
 		return false;
 
-	// Do the initial resize code.
 	OnResize();
 
 	return true;
@@ -92,17 +88,14 @@ void Graphic::OnResize()
 	assert(_swapChain);
 	assert(_directCmdListAlloc);
 
-	// Flush before changing any resources.
 	FlushCommandQueue();
 
 	ThrowIfFailed(_commandList->Reset(_directCmdListAlloc.Get(), nullptr));
 
-	// Release the previous resources we will be recreating.
 	for (int i = 0; i < _SwapChainBufferCount; ++i)
 		_swapChainBuffer[i].Reset();
 	_depthStencilBuffer.Reset();
 
-	// Resize the swap chain.
 	ThrowIfFailed(_swapChain->ResizeBuffers(
 		_SwapChainBufferCount,
 		_appDesc.clientWidth, _appDesc.clientHeight,
@@ -119,7 +112,6 @@ void Graphic::OnResize()
 		rtvHeapHandle.Offset(1, _rtvDescriptorSize);
 	}
 
-	// Create the depth/stencil buffer and view.
 	D3D12_RESOURCE_DESC depthStencilDesc;
 	depthStencilDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
 	depthStencilDesc.Alignment = 0;
@@ -128,11 +120,6 @@ void Graphic::OnResize()
 	depthStencilDesc.DepthOrArraySize = 1;
 	depthStencilDesc.MipLevels = 1;
 
-	// Correction 11/12/2016: SSAO chapter requires an SRV to the depth buffer to read from 
-	// the depth buffer.  Therefore, because we need to create two views to the same resource:
-	//   1. SRV format: DXGI_FORMAT_R24_UNORM_X8_TYPELESS
-	//   2. DSV Format: DXGI_FORMAT_D24_UNORM_S8_UINT
-	// we need to create the depth buffer resource with a typeless format.  
 	depthStencilDesc.Format = DXGI_FORMAT_R24G8_TYPELESS;
 
 	depthStencilDesc.SampleDesc.Count = _appDesc._4xMsaaState ? 4 : 1;
@@ -152,7 +139,6 @@ void Graphic::OnResize()
 		&optClear,
 		IID_PPV_ARGS(_depthStencilBuffer.GetAddressOf())));
 
-	// Create descriptor to mip level 0 of entire resource using the format of the resource.
 	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc;
 	dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
 	dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
@@ -160,19 +146,15 @@ void Graphic::OnResize()
 	dsvDesc.Texture2D.MipSlice = 0;
 	_device->CreateDepthStencilView(_depthStencilBuffer.Get(), &dsvDesc, DepthStencilView());
 
-	// Transition the resource from its initial state to be used as a depth buffer.
 	_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(_depthStencilBuffer.Get(),
 		D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_DEPTH_WRITE));
 
-	// Execute the resize commands.
 	ThrowIfFailed(_commandList->Close());
 	ID3D12CommandList* cmdsLists[] = { _commandList.Get() };
 	_commandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
 
-	// Wait until resize is complete.
 	FlushCommandQueue();
 
-	// Update the viewport transform to cover the client area.
 	_screenViewport.TopLeftX = 0;
 	_screenViewport.TopLeftY = 0;
 	_screenViewport.Width = static_cast<float>(_appDesc.clientWidth);
@@ -238,9 +220,7 @@ LRESULT Graphic::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			GAMEAPP->SetAppStatus(appStatus);
 			return 0;
 
-			// WM_SIZE is sent when the user resizes the window.  
 		case WM_SIZE:
-			// Save the new client area dimensions.
 			_appDesc.clientWidth = LOWORD(lParam);
 			_appDesc.clientHeight = HIWORD(lParam);
 			if (_device)
@@ -260,8 +240,6 @@ LRESULT Graphic::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				}
 				else if (wParam == SIZE_RESTORED)
 				{
-
-					// Restoring from minimized state?
 					if (appStatus.minimized)
 					{
 						appStatus.appPaused = false;
@@ -269,7 +247,6 @@ LRESULT Graphic::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 						OnResize();
 					}
 
-					// Restoring from maximized state?
 					else if (appStatus.maximized)
 					{
 						appStatus.appPaused = false;
@@ -278,16 +255,9 @@ LRESULT Graphic::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 					}
 					else if (appStatus.resizing)
 					{
-						// If user is dragging the resize bars, we do not resize 
-						// the buffers here because as the user continuously 
-						// drags the resize bars, a stream of WM_SIZE messages are
-						// sent to the window, and it would be pointless (and slow)
-						// to resize for each WM_SIZE message received from dragging
-						// the resize bars.  So instead, we reset after the user is 
-						// done resizing the window and releases the resize bars, which 
-						// sends a WM_EXITSIZEMOVE message.
+
 					}
-					else // API call such as SetWindowPos or mSwapChain->SetFullscreenState.
+					else
 					{
 						OnResize();
 					}
@@ -296,7 +266,6 @@ LRESULT Graphic::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			GAMEAPP->SetAppStatus(appStatus);
 			return 0;
 
-			// WM_EXITSIZEMOVE is sent when the user grabs the resize bars.
 		case WM_ENTERSIZEMOVE:
 			appStatus.appPaused = true;
 			appStatus.resizing = true;
@@ -304,8 +273,6 @@ LRESULT Graphic::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			GAMEAPP->SetAppStatus(appStatus);
 			return 0;
 
-			// WM_EXITSIZEMOVE is sent when the user releases the resize bars.
-			// Here we reset everything based on the new window dimensions.
 		case WM_EXITSIZEMOVE:
 			appStatus.appPaused = false;
 			appStatus.resizing = false;
@@ -314,18 +281,13 @@ LRESULT Graphic::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			OnResize();
 			return 0;
 
-			// WM_DESTROY is sent when the window is being destroyed.
 		case WM_DESTROY:
 			PostQuitMessage(0);
 			return 0;
 
-			// The WM_MENUCHAR message is sent when a menu is active and the user presses 
-			// a key that does not correspond to any mnemonic or accelerator key. 
 		case WM_MENUCHAR:
-			// Don't beep when we alt-enter.
 			return MAKELRESULT(0, MNC_CLOSE);
 
-			// Catch this message so to prevent the window from becoming too small.
 		case WM_GETMINMAXINFO:
 			((MINMAXINFO*)lParam)->ptMinTrackSize.x = 200;
 			((MINMAXINFO*)lParam)->ptMinTrackSize.y = 200;
@@ -375,7 +337,6 @@ bool Graphic::InitMainWindow()
 		return false;
 	}
 
-	// Compute window rectangle dimensions based on requested client area dimensions.
 	RECT R = { 0, 0, _appDesc.clientWidth, _appDesc.clientHeight };
 	AdjustWindowRect(&R, WS_OVERLAPPEDWINDOW, false);
 	int width = R.right - R.left;
@@ -398,7 +359,6 @@ bool Graphic::InitMainWindow()
 bool Graphic::InitDirect3D()
 {
 	#if defined(DEBUG) || defined(_DEBUG) 
-		// Enable the D3D12 debug layer.
 		{
 			ComPtr<ID3D12Debug> debugController;
 			ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)));
@@ -408,13 +368,11 @@ bool Graphic::InitDirect3D()
 
 	ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory)));
 
-	// Try to create hardware device.
 	HRESULT hardwareResult = D3D12CreateDevice(
-		nullptr,             // default adapter
+		nullptr,
 		D3D_FEATURE_LEVEL_11_0,
 		IID_PPV_ARGS(&_device));
 
-	// Fallback to WARP device.
 	if (FAILED(hardwareResult))
 	{
 		ComPtr<IDXGIAdapter> pWarpAdapter;
@@ -432,10 +390,6 @@ bool Graphic::InitDirect3D()
 	_rtvDescriptorSize = _device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 	_dsvDescriptorSize = _device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
 	_cbvSrvUavDescriptorSize = _device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
-
-	// Check 4X MSAA quality support for our back buffer format.
-	// All Direct3D 11 capable devices support 4X MSAA for all render 
-	// target formats, so we only need to check quality support.
 
 	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLevels;
 	msQualityLevels.Format = _backBufferFormat;
@@ -475,19 +429,15 @@ void Graphic::CreateCommandObjects()
 	ThrowIfFailed(_device->CreateCommandList(
 		0,
 		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		_directCmdListAlloc.Get(), // Associated command allocator
-		nullptr,                   // Initial PipelineStateObject
+		_directCmdListAlloc.Get(),
+		nullptr,
 		IID_PPV_ARGS(_commandList.GetAddressOf())));
 
-	// Start off in a closed state.  This is because the first time we refer 
-	// to the command list we will Reset it, and it needs to be closed before
-	// calling Reset.
 	_commandList->Close();
 }
 
 void Graphic::CreateSwapChain()
 {
-	// Release the previous swapchain we will be recreating.
 	_swapChain.Reset();
 
 	DXGI_SWAP_CHAIN_DESC sd;
@@ -507,7 +457,6 @@ void Graphic::CreateSwapChain()
 	sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
 
-	// Note: Swap chain uses queue to perform flush.
 	ThrowIfFailed(_dxgiFactory->CreateSwapChain(
 		_commandQueue.Get(),
 		&sd,
@@ -516,23 +465,16 @@ void Graphic::CreateSwapChain()
 
 void Graphic::FlushCommandQueue()
 {
-	// Advance the fence value to mark commands up to this fence point.
 	_currentFence++;
 
-	// Add an instruction to the command queue to set a new fence point.  Because we 
-	// are on the GPU timeline, the new fence point won't be set until the GPU finishes
-	// processing all the commands prior to this Signal().
 	ThrowIfFailed(_commandQueue->Signal(_fence.Get(), _currentFence));
 
-	// Wait until the GPU has completed commands up to this fence point.
 	if (_fence->GetCompletedValue() < _currentFence)
 	{
 		HANDLE eventHandle = CreateEventEx(nullptr, false, false, EVENT_ALL_ACCESS);
 
-		// Fire event when GPU hits current fence.  
 		ThrowIfFailed(_fence->SetEventOnCompletion(_currentFence, eventHandle));
 
-		// Wait until the GPU hits current fence event is fired.
 		WaitForSingleObject(eventHandle, INFINITE);
 		CloseHandle(eventHandle);
 	}
@@ -558,19 +500,14 @@ D3D12_CPU_DESCRIPTOR_HANDLE Graphic::DepthStencilView()const
 
 void Graphic::CalculateFrameStats()
 {
-	// Code computes the average frames per second, and also the 
-	// average time it takes to render one frame.  These stats 
-	// are appended to the window caption bar.
-
 	static int frameCnt = 0;
 	static float timeElapsed = 0.0f;
 
 	frameCnt++;
 
-	// Compute averages over one second period.
 	if ((TIME->TotalTime() - timeElapsed) >= 1.0f)
 	{
-		float fps = (float)frameCnt; // fps = frameCnt / 1
+		float fps = (float)frameCnt;
 		float mspf = 1000.0f / fps;
 
 		wstring fpsStr = to_wstring(fps);
@@ -582,7 +519,6 @@ void Graphic::CalculateFrameStats()
 
 		SetWindowText(_hMainWnd, windowText.c_str());
 
-		// Reset for next average.
 		frameCnt = 0;
 		timeElapsed += 1.0f;
 	}
@@ -592,13 +528,13 @@ void Graphic::LogAdapters()
 {
 	UINT i = 0;
 	IDXGIAdapter* adapter = nullptr;
-	std::vector<IDXGIAdapter*> adapterList;
+	vector<IDXGIAdapter*> adapterList;
 	while (_dxgiFactory->EnumAdapters(i, &adapter) != DXGI_ERROR_NOT_FOUND)
 	{
 		DXGI_ADAPTER_DESC desc;
 		adapter->GetDesc(&desc);
 
-		std::wstring text = L"***Adapter: ";
+		wstring text = L"***Adapter: ";
 		text += desc.Description;
 		text += L"\n";
 
@@ -625,7 +561,7 @@ void Graphic::LogAdapterOutputs(IDXGIAdapter* adapter)
 		DXGI_OUTPUT_DESC desc;
 		output->GetDesc(&desc);
 
-		std::wstring text = L"***Output: ";
+		wstring text = L"***Output: ";
 		text += desc.DeviceName;
 		text += L"\n";
 		OutputDebugString(text.c_str());
@@ -643,20 +579,19 @@ void Graphic::LogOutputDisplayModes(IDXGIOutput* output, DXGI_FORMAT format)
 	UINT count = 0;
 	UINT flags = 0;
 
-	// Call with nullptr to get list count.
 	output->GetDisplayModeList(format, flags, &count, nullptr);
 
-	std::vector<DXGI_MODE_DESC> modeList(count);
+	vector<DXGI_MODE_DESC> modeList(count);
 	output->GetDisplayModeList(format, flags, &count, &modeList[0]);
 
 	for (auto& x : modeList)
 	{
 		UINT n = x.RefreshRate.Numerator;
 		UINT d = x.RefreshRate.Denominator;
-		std::wstring text =
-			L"Width = " + std::to_wstring(x.Width) + L" " +
-			L"Height = " + std::to_wstring(x.Height) + L" " +
-			L"Refresh = " + std::to_wstring(n) + L"/" + std::to_wstring(d) +
+		wstring text =
+			L"Width = " + to_wstring(x.Width) + L" " +
+			L"Height = " + to_wstring(x.Height) + L" " +
+			L"Refresh = " + to_wstring(n) + L"/" + to_wstring(d) +
 			L"\n";
 
 		::OutputDebugString(text.c_str());
--- Engine/MathHelper.cpp
+++ Engine/MathHelper.cpp
@@ -1,9 +1,9 @@
 #include "pch.h"
 #include "MathHelper.h"
 
-DirectX::XMFLOAT4X4 MathHelper::Identity4x4()
+XMFLOAT4X4 MathHelper::Identity4x4()
 {
-	static DirectX::XMFLOAT4X4 I(
+	static XMFLOAT4X4 I(
 		1.0f, 0.0f, 0.0f, 0.0f,
 		0.0f, 1.0f, 0.0f, 0.0f,
 		0.0f, 0.0f, 1.0f, 0.0f,

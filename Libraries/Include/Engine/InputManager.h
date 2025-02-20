e24e393c910e122bc7686f87623a9981ccd509b9
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2025-02-21 01:17:38 +0900

    feat: InputManager 알파벳 값 일괄 추가

--- Libraries/Include/Engine/InputManager.h
+++ Libraries/Include/Engine/InputManager.h
@@ -3,10 +3,31 @@
 enum KeyValue
 {
 	A = 'A',
+	B = 'B',
+	C = 'C',
 	D = 'D',
+	E = 'E',
+	F = 'F',
+	G = 'G',
+	H = 'H',
+	I = 'I',
+	J = 'J',
+	K = 'K',
+	L = 'L',
+	M = 'M',
+	N = 'N',
+	O = 'O',
+	P = 'P',
+	Q = 'Q',
+	R = 'R',
 	S = 'S',
-	W = 'W',
+	T = 'T',
+	U = 'U',
 	V = 'V',
+	W = 'W',
+	X = 'X',
+	Y = 'Y',
+	Z = 'Z',
 	ESC = VK_ESCAPE
 };
 

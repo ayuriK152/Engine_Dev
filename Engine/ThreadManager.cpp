9943dbe343d947da82bb4e4606713dee43d3d2ed
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2026-02-02 18:03:34 +0900

    refactor: 스레드 매니저 스레드 할당 갯수 수정

--- Engine/ThreadManager.cpp
+++ Engine/ThreadManager.cpp
@@ -18,7 +18,11 @@ ThreadManager::~ThreadManager()
 
 void ThreadManager::Init()
 {
-	_numThreads = thread::hardware_concurrency();
+	// 싱글 스레드 환경에서도 해당 기능이 정상작동 하는지 확인이 안된 상황임.
+	// 스레드 개수에 따라 성능이 어떻게 달라지는지 확인해 볼 필요가 있기는 함.
+	// 일단은 전체 스레드 중 75%만 할당하도록 설정
+	_numThreads = thread::hardware_concurrency() / 4 * 3;
+	_numThreads = _numThreads > 0 ? _numThreads : 1;
 
 	for (int i = 0; i < _numThreads; ++i) {
 		_threads.emplace_back([this] {
